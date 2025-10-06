44837f5cf729047521161f730f8f26bbee3cdca2
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-10-06 18:36:31 -0500

    Update iris.py

--- iris.py
+++ iris.py
@@ -33,7 +33,7 @@ elif opcion == 'CSV':
     archivo = st.file_uploader("Selecciona un archivo CSV", type="csv")
     
     if archivo is not None:
-        datos = pd.read_csv(archivo)
+        datos = pd.read_csv(archivo, "irirsnuevo.csv")
         st.write("Datos cargados:")
         st.dataframe(datos)
     else:
